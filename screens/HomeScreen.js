8687fe43b7d4d28370eb1af9950f2359ec374b07
munirudreamer <munirudreamer@gmail.com>
2024-10-21 11:08:47 +0000

    Created components, screens and navigation folders and imported HomeScreen to app.js

---
+++ screens/HomeScreen.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function HomeScreen() {
+  return (
+    <View className="flex-1 mt-[50px]">
+      <Text className="items-center justify-center text-[#6767ed]">HomeScreen</Text>
+    </View>
+  )
+}

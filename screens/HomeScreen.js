47ad3cc31d38bdd457fd40fe2cbe5d23f30c7c2c
munirudreamer <munirudreamer@gmail.com>
2025-04-16 19:58:11 +0000

    updated the home page

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,7 +4,7 @@ import React from 'react'
 export default function HomeScreen() {
   return (
     <View className="flex-1 mt-[50px]">
-      <Text className="items-center justify-center text-[#6767ed]">HomeScreen</Text>
+      <Text className="items-center justify-center text-[#6767ed]">Home Screen</Text>
     </View>
   )
 }
